2f3fee6a3784172491af7e38270676aea925b054
Yamini Settipalli <139889042+yamini749@users.noreply.github.com>
2024-10-02 20:56:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,24 +3,29 @@ import joblib
 
 app = Flask(__name__)
 model = joblib.load("modelspam.joblib")
-vectorizer = joblib.load("tfidfvectorizer.joblib")  
+vectorizer = joblib.load("tfidfvectorizer.joblib")
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', prediction=None)  # Initialize prediction as None
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        email_text = request.form['email_text']
-        
-        # Transform the email text using the loaded TfidfVectorizer
-        data = vectorizer.transform([email_text]).toarray()
-        
-        prediction = model.predict(data)
-        
-        result = 'Spam' if prediction[0] == 1 else 'Not Spam'
-        
-        return render_template('index.html', prediction=result)
+    email_text = request.form['email_text']
+    data = vectorizer.transform([email_text]).toarray()
+    
+    # Get prediction probability
+    prediction_proba = model.predict_proba(data)
+    
+    # Use adjusted threshold
+    adjusted_threshold = 0.4  # Set your new threshold here
+    prediction = (prediction_proba[0][1] >= adjusted_threshold).astype(int)
+    
+    # Determine the result
+    result = 'Spam' if prediction == 0 else 'Ham'
+    
+    # Render the index template with the prediction result
+    return render_template('index.html', prediction=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=3000,debug=True)
